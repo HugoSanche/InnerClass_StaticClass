527e4eec0a89328fab3629a1783830ee31eb10a4
HugoSanche <hbmclaren@gmail.com>
2024-03-02 11:59:03 -0600

    add example static class

--- InnerClasses/src/Main.java
+++ InnerClasses/src/Main.java
@@ -5,5 +5,16 @@ public class Main {
 
         //InnerClass.InsideOtherClass test= new InsideOtherClass();
 
+
+        StaticClass.ExampleStaticClass exampleStaticClass=new StaticClass.ExampleStaticClass();
+        exampleStaticClass.method_B();
+
+        //error
+        //InnerClass.InsideOtherClass insideOtherClass=InnerClass().New InsideOtherClass();
+
+        //CHECA como hacer referencia a la clase que esta dentro de la clase padre.
+        InnerClass innerClass1=new InnerClass();
+        InnerClass.InsideOtherClass insideOtherClass=innerClass1.new InsideOtherClass();
+
     }
 }
--- InnerClasses/src/StaticClass.java
+++ InnerClasses/src/StaticClass.java
@@ -1,16 +1,41 @@
 public class StaticClass {
-    private int a1;
-    private final int a2=0;
-    public int a3=3;
+    private int A_UNO;
+    private final int A_DOS=0;
+    public int A_TRES=3;
 
     public void method_A(){
-        int x1;
-        final int x2=0;
-        int x3;
+        int X_1;
+        final int X_2=0;
+        int X_3;
     }
 
-    public static ExampleStaticClass{
+    public static class ExampleStaticClass{
+        private int B_UNO;
+        private final int B_DOS=2;
+        public int B_TRES;
 
+        public void method_B(){
+            int Y_1;
+            final int Y_2=0;
+            int Y_3=3;
+
+            //class padre
+            //Desde una clase Static NO puedes acceder a los valores de la clase padre
+          //  A_UNO=A_UNO+3;
+          //  System.out.println(A_DOS);
+           // A_TRES=3;
+
+            //Asi puedes acceder a los valores de la clase padre
+            StaticClass staticClass= new StaticClass();
+            staticClass.A_UNO= staticClass.A_UNO+3;
+            System.out.println(staticClass.A_DOS);
+            staticClass.A_TRES=3;
+
+            //This class
+            B_UNO=B_UNO+4;
+            System.out.println(B_DOS);
+            B_TRES=5;
+        }
     }
-
+    ExampleStaticClass exampleStaticClass=new ExampleStaticClass();
 }
